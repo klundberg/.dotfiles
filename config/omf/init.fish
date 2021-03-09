a7e703aff332f04fab0417e09aac953ca1ed0bd4
Kevin Lundberg <klundberg@starry.com>
2021-03-09 14:05:37 -0500

    Update some config, remove unneeded plugins from omf

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,12 +1,17 @@
 # customize user paths
 set --global SSH_AUTH_SOCK (realpath ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh)
-set --global fish_user_paths ~/bin (realpath $OMF_CONFIG/../../bin) ~/.fastlane/bin /usr/local/sbin
+set --global fish_user_paths ~/bin (realpath $OMF_CONFIG/../../bin) ~//Library/Android/sdk/platform-tools/ ~/.fastlane/bin /usr/local/sbin
 
 set --global theme_display_ruby no
 
+# Java
+set --global JAVA_HOME (/usr/libexec/java_home)
+# # Android
+# export PATH=$PATH:/Users/ckeenan/Library/Android/sdk/platform-tools/
+
 # aliases
 alias ls='ls -G'
-alias unfuckxcode='rm -rf ~/Library/Developer/Xcode/DerivedData; rm -rf ~/Library/Caches/com.apple.dt.Xcode; echo -e "\xF0\x9F\x94\xA5 BURN IT ALL\xF0\x9F\x94\xA5"'
+alias unfuckxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/ModuleCache.noindex; rm -rf ~/Library/Developer/Xcode/DerivedData/*/Build; rm -rf ~/Library/Developer/Xcode/DerivedData/*/Index; rm -rf ~/Library/Caches/com.apple.dt.Xcode; echo -e "\xF0\x9F\x94\xA5 BURN IT ALL\xF0\x9F\x94\xA5"'
 
 # setup asdf
 set --global ASDF_DIR (brew --prefix asdf)
