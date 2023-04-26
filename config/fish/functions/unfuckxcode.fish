887b8e5b4b7b5190f9393662bb34437e6411eee5
Kevin Lundberg <klundberg@starry.com>
2023-04-25 23:21:24 -0400

    fewer asdf tools, vscode = git difftool, fix unfuckxcode

--- config/fish/functions/unfuckxcode.fish
+++ config/fish/functions/unfuckxcode.fish
@@ -1,7 +1,7 @@
 function unfuckxcode --description="Deletes lots of derived data folders"
-  rm -rf ~/Library/Developer/Xcode/DerivedData/ModuleCache.noindex
-  rm -rf ~/Library/Developer/Xcode/DerivedData/*/Build
-  rm -rf ~/Library/Developer/Xcode/DerivedData/*/Index
-  rm -rf ~/Library/Caches/com.apple.dt.Xcode
+  rm -rf "$HOME/Library/Developer/Xcode/DerivedData/ModuleCache.noindex"
+  rm -rf "$HOME/Library/Developer/Xcode/DerivedData/*/Build"
+  rm -rf "$HOME/Library/Developer/Xcode/DerivedData/*/Index"
+  rm -rf "$HOME/Library/Caches/com.apple.dt.Xcode"
   echo -e "\xF0\x9F\x94\xA5 BURN IT ALL\xF0\x9F\x94\xA5"
 end
