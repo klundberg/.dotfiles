70765fd3610d278a169214ebcc168718fa1fb90f
Kevin Lundberg <kevin@klundberg.com>
2024-12-06 00:31:18 -0500

    more link tweaks

--- setup/update-symlinks.sh
+++ setup/update-symlinks.sh
@@ -7,10 +7,10 @@ ln -sFf ~/.dotfiles/bashrc ~/.bashrc
 ln -sFf ~/.dotfiles/zprofile ~/.zprofile
 ln -sFf ~/.dotfiles/zshrc ~/.zshrc
 ln -sFf ~/.dotfiles/zshenv ~/.zshenv
-ln -sFf ~/.dotfiles/tool-versions ~/.tool-versions
 ln -sFf ~/.dotfiles/bash_profile ~/.bash_profile
 ln -sFf ~/.dotfiles/Brewfile ~/.Brewfile
 # directories
 mkdir ~/.config &> /dev/null || true 
 ln -sFfh ~/.dotfiles/config/fish ~/.config/fish
 ln -sFfh ~/.dotfiles/config/omf ~/.config/omf
+ln -sFfh ~/.dotfiles/config/mise ~/.config/mise
